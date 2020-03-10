1d66cc1e51c235579da9762cdd0c0ad035d32a0d
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 23:12:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   reverse_sentence = sentence.reverse
   reverse_sentence_array = reverse_sentence.split
-  reverse_sentence_array[::-1]
+  reverse_sentence_array[:,:,-1]
 end
