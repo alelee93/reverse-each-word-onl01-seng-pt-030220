4739795d78bebbfc0cf273933b06043a806dd951
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 23:36:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,17 +3,21 @@ def reverse_each_word(sentence)
   reverse_sentence_array = reverse_sentence.split
   new_array = []
   
-  
   reverse_sentence_array.each do |word|
     new_array.unshift(word)
   end
   new_array.join(" ")
  end
 
+## using collect
+
 second_array =[]
 
-####  
-def reverse_each_word_2(sentence)
+def reverse_each_word(sentence)
+  reverse_sentence = sentence.reverse
+  reverse_sentence_array = reverse_sentence.split
+  second_array = []
+  
   reverse_sentence_array.collect do |word|
    update = second_array.unshift(word)
    return update.join(" ")
