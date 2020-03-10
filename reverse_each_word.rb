e16b864c58d27021055d125eebc4fc5a8b2c2443
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-10 23:15:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(sentence)
   reverse_sentence = sentence.reverse
   reverse_sentence_array = reverse_sentence.split
-  reverse_sentence_array[:,:,-1]
+  new_array = []
+  
+  reverse_sentence_array.each do |word|
+    new_array.unshift(word)
+  end
+  new_array
+    
 end
